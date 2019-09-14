58043e67f71633a79f550857da2e46f4ac59628a
Swati Rout <swati_routr@epam.com>
2019-09-14 15:45:01 +0530

    Loader info plist

--- Application/SmartDiet/SmartDiet/LogIn/SDLogInController.swift
+++ Application/SmartDiet/SmartDiet/LogIn/SDLogInController.swift
@@ -8,23 +8,60 @@
 
 import UIKit
 
-class SDLogInController: UIViewController {
+class SDLogInController: UIViewController,UITextFieldDelegate{
 
+    @IBOutlet weak var usernameTxtFiled: UITextField!
+    
+    @IBOutlet weak var passwordTxtField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func loginClicked(_ sender: Any) {
+        let sv = UIViewController.displaySpinner(onView: self.view)
 
-    /*
-    // MARK: - Navigation
+        logInAPICallwith(username:usernameTxtFiled.text ?? "",password:passwordTxtField.text ?? "")
+        loadHomeScreen()
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
     }
-    */
+    func  logInAPICallwith(username:String,password:String){
+        var params = [
+            "email_id":"swati_rout@epam.com",
+            "password":"swati123"
+        ]
+       var  urlstring = "http://10.71.164.4:8000/authenticate/login/"
+        if let url = URL(string:urlstring){
+            var request = NSMutableURLRequest(url: url)
+            
+           // request.HTTPBody = JSONSerialization.dataWithJSONObject(params, options: nil)
+            
+            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+            //let task = URLSession.shared.dataTask(with: request, completionHandler: { (data, response, error) in
+//                if let data = data {
+//                    if let stringData = String(data: data, encoding: String.Encoding.utf8) {
+//                        print(stringData)
+//                        //UIViewController.removeSpinner(spinner: sv)
+//
+//                        //JSONSerialization
+//                    }
+//                }
+//            })
+            //task.resume()
+        }
+        
+        
+       
+    }
+
+    func loadHomeScreen(){
+//        let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+//        let loggedInViewController = storyBoard.instantiateViewController(withIdentifier: "LoggedInViewController") as! LoggedInViewController
+//        self.present(loggedInViewController, animated: true, completion: nil)
+    }
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+    }
 
 }
