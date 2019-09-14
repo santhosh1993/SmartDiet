0c6c7ec133a71d60782bd7284c04e634619129da
Swati Rout <swati_routr@epam.com>
2019-09-14 12:58:35 +0530

    Log In Screen Design

---
+++ Application/SmartDiet/SmartDiet/LogIn/SDLogInController.swift
@@ -0,0 +1,30 @@
+//
+//  LogInController.swift
+//  SmartDiet
+//
+//  Created by Swati on 14/09/19.
+//  Copyright © 2019 Epam. All rights reserved.
+//
+
+import UIKit
+
+class SDLogInController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
