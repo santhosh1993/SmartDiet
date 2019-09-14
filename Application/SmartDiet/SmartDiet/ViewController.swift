00bc6360f922162c862704965c655f1568d60069
suneelkumargavara <sunilkumargavara@gmail.com>
2019-09-14 10:19:03 +0530

    1. Adding iOS project to the repository

---
+++ Application/SmartDiet/SmartDiet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SmartDiet
+//
+//  Created by Sunilkumar Gavara on 14/09/19.
+//  Copyright © 2019 Epam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
