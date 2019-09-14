02de126d92f52b404b2da97248f91e5fe6ecdded
Santhosh <santhosh_nampally@epam.com>
2019-09-14 18:55:05 +0530

    added update of a meal

--- Backend/Django/SmartDiet/DummyData/api_views.py
+++ Backend/Django/SmartDiet/DummyData/api_views.py
@@ -34,9 +34,7 @@ class SuggestAMealAPIView(APIView):
         classifications = new_model.predict([[calories, sleeptime, illness, foodtime]])
         result = (classifications > 0.5).astype(np.int)
         value = np.where(result[0] == 1)[0][0]
-        print(value)
-        print(result)
-        print("******")
+
         food = Food.objects.filter(food_id=value)
 
         if food.exists():
@@ -44,4 +42,23 @@ class SuggestAMealAPIView(APIView):
         else:
             return Response({"message": "We could not suggest you this time", "status_code": 2002}, HTTP_200_OK)
 
+class UpdateAMealAPIView(APIView):
+    def post(self, request, format=None):
+        calories = request.data.get('calories')
+        sleeptime = request.data.get('sleeptime')
+        food_id = request.data.get('food_id')
+        foodtime = request.data.get('foodtime')
+        illness = request.data.get('illness')
 
+        new_model = keras.models.load_model(
+            '/Users/santhosh_nampally/Projects/smartdiet/Backend/Django/SmartDiet/DummyData/my_model_update.h5')
+        classifications = new_model.predict([[calories, sleeptime, food_id, foodtime]])
+        result = (classifications > 0.5).astype(np.int)
+        value = np.where(result[0] == 1)[0][0]
+        print(value)
+        print(result)
+        print("****")
+        if value == illness:
+            return Response({"message": "Selected food is apt according to your health", "status_code": 2003}, HTTP_200_OK)
+        else:
+            return Response({"message": "Selected food is not so apt according to your health", "status_code": 2004}, HTTP_200_OK)
--- Backend/Django/SmartDiet/DummyData/urls.py
+++ Backend/Django/SmartDiet/DummyData/urls.py
@@ -5,5 +5,6 @@ from . import api_views
 urlpatterns = [
     path('insert/', views.index, name="insert data"),
     path('foodlist/', api_views.FoodList.as_view(), name="food list"),
-    path('suggestme/', api_views.SuggestAMealAPIView.as_view(), name="suggest food list")
+    path('suggestme/', api_views.SuggestAMealAPIView.as_view(), name="suggest food list"),
+    path('updateme/', api_views.UpdateAMealAPIView.as_view(), name="update food list")
 ]
