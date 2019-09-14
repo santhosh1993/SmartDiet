f9c5dbd2523e8bc65eb40450e92bdb2a44605238
Santhosh <santhosh_nampally@epam.com>
2019-09-14 12:43:37 +0530

    added users model and admin page

--- Backend/Django/SmartDiet/SmartDiet/settings.py
+++ Backend/Django/SmartDiet/SmartDiet/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'DummyData.apps.DummydataConfig',
     'rest_framework',
+    'SmartDietUser.apps.SmartdietuserConfig',
 ]
 
 MIDDLEWARE = [
--- Backend/Django/SmartDiet/SmartDietUser/models.py
+++ Backend/Django/SmartDiet/SmartDietUser/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Users(models.Model):
+    user_name = models.CharField(max_length=50)
+    email_id = models.CharField(max_length=50, unique=True)
+    password = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.user_name}"
