d81763f03ecf2fb2cd6c1f522d83cd2212787b38
suneelkumargavara <sunilkumargavara@gmail.com>
2019-09-15 12:38:12 +0530

    Adding missed changes

---
+++ Application/SmartDiet/SmartDiet/CustomCollectionCells/MealDetail/CellDescription.swift
@@ -0,0 +1,18 @@
+//
+//  CellDescription.swift
+//  SmartDiet
+//
+//  Created by Sunilkumar Gavara on 15/09/19.
+//  Copyright © 2019 Epam. All rights reserved.
+//
+
+import UIKit
+
+class CellDescription: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
