c36194c13844d6c754a4b58e61ea0f3e91b4ac93
suneelkumargavara <sunilkumargavara@gmail.com>
2019-09-15 09:01:09 +0530

    1. Added Meal Detail View along with its custom components

---
+++ Application/SmartDiet/SmartDiet/CustomCollectionCells/MealDetail/CellCharts.swift
@@ -0,0 +1,18 @@
+//
+//  CellCharts.swift
+//  SmartDiet
+//
+//  Created by Sunilkumar Gavara on 14/09/19.
+//  Copyright © 2019 Epam. All rights reserved.
+//
+
+import UIKit
+
+class CellCharts: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ Application/SmartDiet/SmartDiet/CustomComponents/MealDetails/NutritionIndicator.swift
@@ -0,0 +1,28 @@
+//
+//  NutritionIndicator.swift
+//  SmartDiet
+//
+//  Created by Sunilkumar Gavara on 15/09/19.
+//  Copyright © 2019 Epam. All rights reserved.
+//
+
+import UIKit
+
+final class NutritionIndicator: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.addViewFromXib()
+    }
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.addViewFromXib()
+    }
+    private func addViewFromXib() {
+        let view = self.getViewFromXibAndAddItAsSubView()
+        if let unWrappedView = view { self.addSubview(unWrappedView) }
+    }
+}
